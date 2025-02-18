a13b8c68c4fb1e86f4c463547ad37fdc86b51135
Jh700wolf <jh700wolf@gmail.com>
2025-02-18 11:57:02 -0600

    Problema 2 Resuelto

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+function contador(){
+
+}
+
+
+
+
 console.log("Problema 1");
 var pregunta1 = prompt("Dame un número:");
 var numeros = "";
@@ -7,7 +14,6 @@ for(let i=1; i<=pregunta1;i++){
 }
 alert("La lista de numeros es: " + numeros);
 document.write(numeros)
-
 console.log("Problema 2");
 var n1 = Math.floor(Math.random()*11);
 var n2 = Math.floor(Math.random()*11);
@@ -15,5 +21,12 @@ var tiempoinicio= new Date();
 var pregunta2 = prompt("Dame la suma de:"+n1+"+"+n2);
 var tiempofinal=new Date();
 
-console.log(tiempoinicio.getSeconds());
-console.log(tiempofinal.getSeconds());
+var tiempoTotal=(tiempofinal.getSeconds()-tiempoinicio.getSeconds());
+if (pregunta2=n1+n2){
+    alert("Tu resultado fue correcto. Tardaste: "+tiempoTotal+" Segundos.");
+}
+else{
+    alert("Tu resultado fue incorrecto. Tardaste: "+tiempoTotal+" Segundos.");
+}
+
+console.log("Problema 3");
