9797202a2c8bca282639805076a8dd714d0f5fd0
Jh700wolf <jh700wolf@gmail.com>
2025-02-20 13:12:44 -0600

    Clase Node Lalo

--- js/app.js
+++ js/app.js
@@ -3,5 +3,9 @@ file_system.writeFileSync('hola.txt', 'Hola desde node');//Este es syncronico
 setTimeout(()=>{console.log("UwU")},10000);
 
 const http = require('http');
-const server = http.createServer((request, response)=>{});
+const server = http.createServer((request, response)=>{
+    response.setHeader('Contemt-Type', 'text.html');
+    response.write("<h1> Ligoleie </h1>");
+    response.end();
+});
 server.listen(3000);
