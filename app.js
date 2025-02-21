2b9b643cfdf83f74319da921749344e833e646cf
Jh700wolf <jh700wolf@gmail.com>
2025-02-21 13:01:40 -0600

    Lab 8 Problema 4

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ function fizzbuzz(){
 var array1= new Array(4,2,7,4,1,2,1,9,8,11);
 const file_system=require('fs');
 file_system.writeFileSync('hola.txt', 'Hola desde node');//Este es syncronico
+var pagina = file_system.readFileSync('index.html');
 escribir("Prueba de problema 2");
 setTimeout(()=>{console.log("UwU")},10000);
 fizzbuzz();
@@ -41,9 +42,7 @@ fizzbuzz();
 const http = require('http');
 const server = http.createServer((request, response)=>{
     console.log(promedio(array1));
-    file_system.readFileSync("index.html");
-    //response.setHeader('Contemt-Type', 'text.html');
-    //response.write("<h1> Laboratorio </h1>");
+    response.write(pagina);
     response.end();
 });
 server.listen(3000);
