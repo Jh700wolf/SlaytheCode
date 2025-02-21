8dc8ac316a02b8dc1cecbd648f398143fe080f6f
Jh700wolf <jh700wolf@gmail.com>
2025-02-20 20:38:18 -0600

    Lab 8 Problema 3

--- js/app.js
+++ js/app.js
@@ -12,17 +12,38 @@ function escribir(str){
     file_system.writeFileSync('Lab8.txt', str);
 }
 
+function fizzbuzz(){
+
+    for (let i = 1; i<=100; i++){
+        if (i%3==0){
+            if(i%5==0){
+                console.log("fizzbuzz")
+            }
+            else{
+                console.log("fizz");
+            }
+        }
+        else if (i%5==0){
+            console.log("buzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
 var array1= new Array(4,2,7,4,1,2,1,9,8,11);
 const file_system=require('fs');
 file_system.writeFileSync('hola.txt', 'Hola desde node');//Este es syncronico
 escribir("Prueba de problema 2");
 setTimeout(()=>{console.log("UwU")},10000);
+fizzbuzz();
 
 const http = require('http');
 const server = http.createServer((request, response)=>{
     console.log(promedio(array1));
-    response.setHeader('Contemt-Type', 'text.html');
-    response.write("<h1> Laboratorio </h1>");
+    file_system.readFileSync("index.html");
+    //response.setHeader('Contemt-Type', 'text.html');
+    //response.write("<h1> Laboratorio </h1>");
     response.end();
 });
 server.listen(3000);
