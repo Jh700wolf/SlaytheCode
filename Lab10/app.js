e9f0d057ae5c2439af6b563f3430708be6cdebbb
Jh700wolf <jh700wolf@gmail.com>
2025-02-25 13:00:16 -0600

    Utilizar middleware

--- Lab10/app.js
+++ Lab10/app.js
@@ -1,4 +1,4 @@
-const html_header = `
+/*const html_header = `
 <!doctype html>
 <html class="no-js" lang="es" dir="ltr">
     <head>
@@ -112,4 +112,69 @@ const server = http.createServer( (request, response) => {
     
 });
 
-server.listen(3000);
+server.listen(3000);*/
+
+
+
+const html_header = `
+<!doctype html>
+<html class="no-js" lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lab 10</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites/dist/css/foundation.min.css">
+    </head>
+    <body>
+        <div class="grid-x">
+            <div class="cell small-6"><h1>Laboratorio 10</h1></div>
+            <div class="cell small-6">
+                <div class="button-group">
+                    <a class="large button" href="https://get.foundation/sites/docs/kitchen-sink.html">Hola</a>
+                    <a class="large button" href="https://get.foundation/sites/docs/xy-grid.html">Adios</a>
+                </div>
+            </div>
+        </div>
+    `;
+const html_form = `        
+<!--FORM-->
+        <div>
+            <form action="/agregar" method="POST">
+                <Label>Nombre:</Label>
+                <input type="text" placeholder="Nombre" id="nombre" name="nombre">
+                <input class="button" type="submit" value="Enviar">
+
+            </form>
+        </div>
+        <div>
+            <form action="/opiniones" method="POST">
+                <Label>Dame tu opinion de la página:</Label>
+                <input type="text" placeholder="Escribe aqui..." id="opinion" name="opinion">
+                <div class="input-group-button">
+                    <input type="submit" class="button" value="Enviar">
+                </div>
+            </form>
+        </div>
+    </body>`;
+
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use('/agregar',(request, response, next)=>{
+  response.send(html_header+html_form);
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta. Es la combinacion de setheader, write y end
+});
+
+app.listen(3000);
+
