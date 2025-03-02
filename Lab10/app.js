9a37c1e75beceba4bb2d383702aa19b4a25b0579
Jh700wolf <jh700wolf@gmail.com>
2025-03-02 12:46:20 -0600

    Crear Lab10 y agregar menu

--- Lab10/app.js
+++ Lab10/app.js
@@ -1,31 +1,121 @@
-const express = require('express');
-const app = express();
+const html_header=  `
+<!doctype html>
+<html class="no-js" lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lab 10</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites/dist/css/foundation.min.css">
+    </head>
+    <body>
+        <div class="grid-x">
+                <div class="top-bar">
+                <div class="top-bar-left">
+                    <ul class="dropdown menu" data-dropdown-menu>
+                    <li class="menu-text">Bienvenido a mi Lab 10!</li>
+                    <li><a href="https://get.foundation/sites/docs/kitchen-sink.html">Kitchen Sink de Foundation</a></li>
+                    <li><a href="https://get.foundation/sites/docs/xy-grid.html">Guia para el xy grid</a></li>
+                    <li><a href="https://get.foundation/sites/docs/dropdown-menu.html">Guia para el menu</a></li>
+                    </ul>
+                </div>
+                <div class="top-bar-right">
+                    <ul class="menu">
+                    <li><input type="search" placeholder="Search"></li>
+                    <li><button type="button" class="button">Search</button></li>
+                    </ul>
+                </div>
+                </div>
+        </div>
+`;
+const html_form=`
+<div>
+    <form action="/agregar/notas" method="POST">
+        <Label>Nombre:</Label>
+        <input type="text" placeholder="Nombre" id="nombre" name="nombre">
+        <input class="button" type="submit" value="Enviar">
 
-const path =require('path')
-app.use(express.static(path.join(__dirname, "public")));
+    </form>
+</div>
+<div>
+    <form action="/agregar/opiniones" method="POST">
+        <Label>Dame tu opinion de la página:</Label>
+        <input type="text" placeholder="Escribe aqui..." id="opinion" name="opinion">
+        <div class="input-group-button">
+            <input type="submit" class="button" value="Enviar">
+        </div>
+    </form>
+</div>
+</body>
+`;
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const html_footer ="";
 
-const bodyParser=require('body-parser');
+const notas = [];
 
-app.use(bodyParser.urlencoded({extended: false}));
+const http = require('http');
 
-//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+const server = http.createServer( (request, response) => {  
+  
+  if(request.method == "GET" && (request.url == "/agregar/notas" || request.url == "/")) {
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write(html_header + html_form + html_footer);
+    response.end();
+  } 
+  else if(request.method == "POST" && request.url=="/agregar/opiniones"){
+    const datos_completos=[];
+    request.on('data',(data)=>{
+        console.log(data);
+        
+    })
+  }
+  
+  else if(request.method == "POST" && request.url == "/agregar/notas") {
+    
+    const datos_completos = [];
+
+    request.on('data', (data)=>{
+      console.log(data);
+      datos_completos.push(data);
+    });
+
+    request.on('end', () => {
+      const string_datos_completos = Buffer.concat(datos_completos).toString();
+      console.log(string_datos_completos);
+      //split() separa un string por el parámetro recibido, 
+      //y cada parte la pone en un arreglo
+      const nueva_nota = string_datos_completos.split('=')[1];
+
+      //Si fueran 2 inputs:
+      //const nueva_planta = string_datos_completos.split('&')[0].split('=')[1];
+
+      notas.push(nueva_nota);
+
+      response.setHeader('Content-Type', 'text/html');
+      response.write(html_header);
+      
+      response.write(`<div class="row">`);
+      for(const nota of notas) {
+        response.write(nota);
+        response.write("<br>")
+      }
+      response.write(`</div>`);
+
+      response.write(html_footer);
+      response.end();
+
+    });
+
+  } else {
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/html');
+    response.write(html_header);
+    response.write('<div class="notification is-danger">La página no existe</div>');
+    response.write(html_footer);
+    response.end();
+  }
+    
 });
 
-const notasRoutes=require('./routes/notas.routes');
-
-app.use("/agregar", notasRoutes);
-
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.statusCode=404;
-    response.send("No se encontro lo que buscas!")
-});
-
-app.listen(3000);
-
+server.listen(3000);
---
+++ Lab11/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const path =require('path')
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+const notasRoutes=require('./routes/notas.routes');
+
+app.use("/agregar", notasRoutes);
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.statusCode=404;
+    response.send("No se encontro lo que buscas!")
+});
+
+app.listen(3000);
+
