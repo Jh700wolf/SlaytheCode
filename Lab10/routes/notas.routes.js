9050daf2875a1e0390298b70f8848d7ab9b351ec
Jh700wolf <jh700wolf@gmail.com>
2025-03-02 10:48:06 -0600

    Clase lalo lab 12

--- Lab10/routes/notas.routes.js
+++ Lab10/routes/notas.routes.js
@@ -2,56 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const html_header = `
-<!doctype html>
-<html class="no-js" lang="es" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Lab 10</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites/dist/css/foundation.min.css">
-    </head>
-    <body>
-        <div class="grid-x">
-            <div class="cell small-6"><h1>Laboratorio 10</h1></div>
-            <div class="cell small-6">
-                <div class="button-group">
-                    <a class="large button" href="https://get.foundation/sites/docs/kitchen-sink.html">Hola</a>
-                    <a class="large button" href="https://get.foundation/sites/docs/xy-grid.html">Adios</a>
-                </div>
-            </div>
-        </div>
-    `;
-const html_form = `        
-<!--FORM-->
-        <div>
-            <form action="/agregar/notas" method="POST">
-                <Label>Nombre:</Label>
-                <input type="text" placeholder="Nombre" id="nombre" name="nombre">
-                <input class="button" type="submit" value="Enviar">
-
-            </form>
-        </div>
-        <div>
-            <form action="/opiniones" method="POST">
-                <Label>Dame tu opinion de la página:</Label>
-                <input type="text" placeholder="Escribe aqui..." id="opinion" name="opinion">
-                <div class="input-group-button">
-                    <input type="submit" class="button" value="Enviar">
-                </div>
-            </form>
-        </div>
-    </body>`;
-
-
-
-
 const notas = [];
 
 //app.get es para registrar un middleware para peticiones http get.
 router.get('/notas',(request, response, next)=>{
-    response.send(html_header+html_form);
+    response.render("agregar_nota");
   });
   
   //app.post es para registrar un middleware para peticiones http post.
