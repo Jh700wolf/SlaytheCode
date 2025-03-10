6dff27cfdb3871790b2ef129ff6ed2bd443559e3
Jh700wolf <jh700wolf@gmail.com>
2025-03-09 20:20:01 -0600

    modelos msv a opiniones

--- Lab11/controllers/opiniones.controller.js
+++ Lab11/controllers/opiniones.controller.js
@@ -1,6 +1,45 @@
+const Opinion = require('../models/opinion.model')
+
+exports.get_opinion = (request, response, next)=>{
+    const mensaje =request.session.info || '';
+    if(request.session.info){
+      request.session.info='';
+    }
+    Opinion.fetchAll(request.params.id)
+    .then(([rows, fieldData]) => {
+      console.log(fieldData);
+      console.log(rows);
+      response.render('mostrar_opiniones',{
+        isLoggedIn:request.session.isLoggedIn || false,
+        username: request.session.username || '',
+        opiniones:rows,
+        info: mensaje,
+      });
+    }).catch((error)=>{
+      console.log(error);
+    });
+};
+
+exports.get_agregar_opinion = (request, response, next)=>{
+    console.log(request.session.username);
+    response.render("menu_principal",{
+      isLoggedIn: request.session.isLoggedIn || false,
+      username: request.session.username||'',
+  });
+  };
+
+
+
 exports.post_agregar_opinion = (request, response, next) => {
     console.log(request.session.username);
-    const mi_opinion=new (request.body.nombre);
+    const mi_opinion=new Opinion(request.body.opinion);
     mi_opinion.save()
+    .then(()=>{
+        response.redirect('/opiniones/ver');
+        console.log("OPINION GUARDADA CORRECTAMENTE");
+      }) //Si la promesa se cumple
+      .catch((error)=>{
+        console.log(error);
+      }); //Si la promesa falla;
 
 }
--- Lab11/routes/opiniones.routes.js
+++ Lab11/routes/opiniones.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const opiniones = [];
+const opiniones_Controller =require('../controllers/opiniones.controller');
 
-router.post('/agregar',(request, response, next)=>{
+router.get('/agregar', opiniones_Controller.get_agregar_opinion)
+router.get('/ver', opiniones_Controller.get_opinion)
+router.post('/agregar', opiniones_Controller.post_agregar_opinion /*(request, response, next)=>{
     console.log(request.body);
     opiniones.push(request.body.opinion);
     const fs=require("node:fs");
@@ -14,6 +16,11 @@ router.post('/agregar',(request, response, next)=>{
       {
         opiniones:opiniones,
       });
-  });
+  }*/
+ 
+ 
+    );
+
+router.get("/", )
 
   module.exports = router;
