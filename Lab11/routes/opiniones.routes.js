021bc3c30d757b3e0bb64edd78f161e0aa0b39ee
Jh700wolf <jh700wolf@gmail.com>
2025-03-11 11:17:13 -0600

    proteger opiniones

--- Lab11/routes/opiniones.routes.js
+++ Lab11/routes/opiniones.routes.js
@@ -1,11 +1,13 @@
+const { response, request}=require('express')
 const express = require('express');
 const router = express.Router();
 
+const isAuth = require('../util/is-auth');
 const opiniones_Controller =require('../controllers/opiniones.controller');
 
-router.get('/agregar', opiniones_Controller.get_agregar_opinion)
-router.get('/ver', opiniones_Controller.get_opinion)
-router.post('/agregar', opiniones_Controller.post_agregar_opinion /*(request, response, next)=>{
+router.get('/agregar', isAuth, opiniones_Controller.get_agregar_opinion)
+router.get('/ver', isAuth, opiniones_Controller.get_opinion)
+router.post('/agregar', isAuth, opiniones_Controller.post_agregar_opinion /*(request, response, next)=>{
     console.log(request.body);
     opiniones.push(request.body.opinion);
     const fs=require("node:fs");
