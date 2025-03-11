2091e83f3caa59619272b803c4dbdfd6b599955c
Jh700wolf <jh700wolf@gmail.com>
2025-03-11 11:02:06 -0600

    Proteger con autentificacion

--- Lab11/routes/notas.routes.js
+++ Lab11/routes/notas.routes.js
@@ -1,17 +1,19 @@
+const { response, request}=require('express')
 const express = require('express');
 const router = express.Router();
 
+const isAuth = require('../util/is-auth');
 
 const notas_controllers=require('../controllers/notas.controllers');
 //app.get es para registrar un middleware para peticiones http get.
-router.get('/agregar',notas_controllers.get_agregar_notas);
+router.get('/agregar', isAuth, notas_controllers.get_agregar_notas);
   
   //app.post es para registrar un middleware para peticiones http post.
-router.post('/agregar', notas_controllers.post_agregar_notas);
+router.post('/agregar', isAuth, notas_controllers.post_agregar_notas);
 
 
 
-router.post('/busqueda',(request, response, next)=>{
+router.post('/busqueda', isAuth,(request, response, next)=>{
   console.log(request.body);
   const busqueda=request.body.busqueda;
   response.render("busqueda_fallida",
